fb4e3d30e72bd37240bb3b560156c82682a20b49
A11MA <emladaug@gmail.com>
2025-06-11 20:10:15 +0300

    labas

---
+++ kazkas/src/test/java/lt/techin/BaseTest.java
@@ -0,0 +1,33 @@
+package lt.techin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseTest
+{
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://teststore.automationtesting.co.uk/index.php");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+   /* @AfterEach
+   public void afterEach()
+    {
+        if(driver != null)
+        {
+            driver.quit();
+        }
+
+    }*/
+}
